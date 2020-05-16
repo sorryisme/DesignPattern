d5a0cef863f25fd3d0450a22261e59d0548e5930
sorryisme <wivipp39@gmail.com>
2020-05-16 23:31:09 +0900

    20200516

--- designPattern/src/oopDesignPattern/ch07/strategy/after/UIController.java
+++ designPattern/src/oopDesignPattern/ch07/strategy/after/UIController.java
@@ -1,5 +1,7 @@
 package oopDesignPattern.ch07.strategy.after;
 
+import java.util.List;
+
 public class UIController {
 
 	private DiscountStrategy strategy;
@@ -8,9 +10,9 @@ public class UIController {
 		strategy = new FirstGuestDiscountStrategy();
 	}
 	
-	public void onCalculatorButtonClick() {
+	public void onCalculatorButtonClick(List<Item> items) {
 		Calculator cal = new Calculator(strategy);
-		int price = cal.
+		int price = cal.calculate(items);
 	}
 	
 }
